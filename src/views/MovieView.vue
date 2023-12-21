b81afbf9448608b8f6f48c61f8fc756106afb274
vakkaskarakurt <karakurtvakkas@gmail.com>
2023-12-21 14:04:03 +0300

    Added singular MovieView

---
+++ src/views/MovieView.vue
@@ -0,0 +1,32 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <MovieComponent :movieObj="currentMovie" />
+</template>
+
+<script>
+import MovieComponent from '../components/MovieComponent.vue';
+export default {
+  components: {
+    MovieComponent
+  },
+  props: ['movieId'],
+  computed: {
+    currentMovie() {
+      const movieObj = this.$store.getters.getCurrentMovie
+      return {
+        movieName: JSON.parse(movieObj).movieName,
+        movieReleaseDate: JSON.parse(movieObj).movieReleaseDate,
+        movieGenre: JSON.parse(movieObj).movieGenres,
+        imageURL: JSON.parse(movieObj).imageURL,
+        movieDuration: JSON.parse(movieObj).movieRuntime,
+        movieDirector: JSON.parse(movieObj).movieDirector,
+        movieWriter: JSON.parse(movieObj).movieWriter,
+        movieScore: JSON.parse(movieObj).movieScore,
+        movieActors: JSON.parse(movieObj).movieActors,
+        description: JSON.parse(movieObj).movieDescription
+      }
+    }
+    // For demo purposes, using Lorem Ipsum for comments
+  }
+}
+</script>
