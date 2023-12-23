280c20894ce483230fddb95393f0ef662f7a6ed8
Kürşat Edabalı Yıldırım <40580360+kedabaliyildirim@users.noreply.github.com>
2023-12-23 11:16:09 +0300

    transferred store operations to app.vue

--- src/main.js
+++ src/main.js
@@ -9,13 +9,6 @@ import store from "./store";
 
 const app = createApp(App);
 
-const createStore = async () => {
-    await store.dispatch('getMovieData', {
-        page: 1,
-        page_size: 20
-    });
-    await store.dispatch('getTopTen');
-};
 
 app.use(router);
 app.use(store);
