17f875030340b9f757d8f9e97727f0d0b38afd4f
klavas35 <sleepy19@hotmail.com>
2023-12-31 02:15:40 +0300

    added a cron job to pariodically call comments at 01:00 AM

---
+++ src/api/cron.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
+
+// jshint ignore:start
+let url;
+let authKey
+url =
+    import.meta.env.VITE_API_URL
+authKey =
+    import.meta.env.VITE_AUTH_TOKEN
+// jshint ignore:end
+const localURL = url;
+const pullComments = async (authCookie, movie) => {
+    const data = {
+        authToken: authCookie,
+        platform: {
+            "reddit": false,
+            "youtube": true,
+        },
+        movie: movie
+    };
+
+    try {
+        console.log("@pullComments");
+        const response = await axios({
+            url: localURL + "comments/pullcomments",
+            method: "POST",
+            data: data,
+            withCredentials: true,
+        });
+
+        console.log("Comments pulled");
+    } catch (error) {
+        console.error('Error in pullComments request:', error);
+    }
+};
+
+const requestMovieNames = async (authCookie) => {
+    const data = {
+        authToken: authCookie
+    };
+
+    try {
+        const response = await axios({
+            url: localURL + "comments/getmovienames",
+            method: "POST",
+            data: data,
+            withCredentials: true,
+        });
+
+        const movieNames = response.data;
+        return movieNames;
+    } catch (error) {
+        console.error('Error in requestMovieNames request:', error);
+        throw error; // Rethrow the error to handle it in the calling function
+    }
+};
+
+const cron_main = async () => {
+    try {
+        
+        const movieNames = await requestMovieNames(authKey);
+        setTimeout(async () => {
+            for (const movie of movieNames) {
+                await pullComments(authKey, movie);
+            }
+        }, 2000);
+
+        console.log("Cron job completed successfully");
+    } catch (error) {
+        console.error('Error in cron_main:', error);
+    }
+};
+
+cron_main(); // Call the cron_main function to execute the cron job
