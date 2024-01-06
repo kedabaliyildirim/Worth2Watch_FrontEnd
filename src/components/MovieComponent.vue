e0bbe13fbcaa94433a4cdada324d22b735b74716
Emrah Fidan <201805070@stu.adu.edu.tr>
2024-01-07 01:08:18 +0300

    comment field improved - youtube removed

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -76,31 +76,7 @@
         <p class="movieDescription"><strong> ✨ </strong>{{ movieObj.description }}</p>
       </div>
 
-      <!-- Youtube start -->
-      <div class="commentSection">
-        <div class="faq-container">
-          <div class="faq-question" @click="toggleFAQ1">
-            <img
-              style="width: 18px; margin-left: 15px"
-              src="@/assets/icons/toogle.svg"
-              alt="My Icon"
-            />
-            YouTube Comments
-            <p class="sentiment-analyze">👍% 52 - 💩 % 48</p>
-            <!-- sentiment analiz -->
-          </div>
-          <div
-            class="faq-answer"
-            v-for="comment in movieObj.movieComments.youtubeComments"
-            :key="comment"
-          >
-            {{ comment.comment }}
-          </div>
-        </div>
-      </div>
-      <!-- Youtube finish -->
-
-      <!-- reddit-start -->
+      <!-- Reddit start -->
       <div class="commentSection">
         <div class="faq-container">
           <div class="faq-question-2" @click="toggleFAQ2">
@@ -109,21 +85,56 @@
               src="@/assets/icons/toogle.svg"
               alt="My Icon"
             />
-            Reddit Comments
-            <p class="sentiment-analyze">👍% 61 - 💩 % 39</p>
-            <!-- sentiment analiz -->
+            <div>
+              <img
+                style="width: 25px; margin-bottom: -7px"
+                src="@/assets/icons/reddit.png"
+                alt="My Icon"
+              />
+              <span> Reddit Comments</span>
+            </div>
+
+            <p class="sentiment-analyze" ref="redditSentiment"></p>
           </div>
-          <div
-            class="faq-answer-2"
-            v-for="comment in movieObj.movieComments.redditComments"
-            :key="comment"
-          >
-            {{ comment.comment }}
-            {{ comment.sentiment }}
-          </div>
+          <!-- Updated loop to create faq-answer-2 only if sentiment is present -->
+          <template v-for="(comment, index) in movieObj.movieComments.redditComments" :key="index">
+            <template v-if="comment.sentiment !== undefined">
+              <div class="faq-answer-2">
+                {{ comment.comment }}
+                <div class="left-aligned">{{ comment.sentiment }}</div>
+              </div>
+            </template>
+          </template>
         </div>
       </div>
       <!-- Reddit finish -->
+
+      <!-- youtube start -->
+      <!-- 
+      <div class="commentSection">
+        <div class="faq-container">
+          <div class="faq-question" @click="toggleFAQ2">
+            <img
+              style="width: 18px; margin-left: 15px"
+              src="@/assets/icons/toogle.svg"
+              alt="My Icon"
+            />
+            YouTube Comments
+            <p class="sentiment-analyze" ref="youtubeSentiment"></p>
+          </div>
+
+          <template v-for="(comment, index) in movieObj.movieComments.youtubeComments" :key="index">
+            <template v-if="comment.sentiment !== undefined">
+              <div class="faq-answer">
+                {{ comment.comment }}
+                <div class="left-aligned">{{ comment.sentiment }}</div>
+              </div>
+            </template>
+          </template>
+        </div>
+      </div> -->
+
+      <!-- youtube finish -->
     </div>
   </div>
 </template>
@@ -135,16 +146,35 @@ export default {
     getProviderLogoPath(logoPath) {
       return `https://image.tmdb.org/t/p/original${logoPath}`
     },
-    toggleFAQ1() {
-      var answers = document.getElementsByClassName('faq-answer')
-      for (var i = 0; i < answers.length; i++) {
-        if (answers[i].style.display === 'none') {
-          answers[i].style.display = 'block'
-        } else {
-          answers[i].style.display = 'none'
-        }
-      }
-    },
+
+    // toggleFAQ1() {
+    //   var answers = document.getElementsByClassName('faq-answer')
+    //   for (var i = 0; i < answers.length; i++) {
+    //     if (answers[i].style.display === 'none') {
+    //       answers[i].style.display = 'block'
+    //     } else {
+    //       answers[i].style.display = 'none'
+    //     }
+    //   }
+
+    //   // Comment sentimenterini toplamak ve ortalama hesaplamak için
+    //   const sentimentList = this.movieObj.movieComments.youtubeComments
+    //     .filter((comment) => comment.sentiment !== undefined) // Filter out comments without sentiment
+    //     .map((comment) => comment.sentiment)
+
+    //   const totalSentiments = sentimentList.length
+    //   const sumOfSentiments = sentimentList.reduce(
+    //     (sum, sentiment) => sum + parseFloat(sentiment),
+    //     0
+    //   )
+
+    //   // Check if there are comments with sentiment to avoid division by zero
+    //   const averageSentiment = totalSentiments > 0 ? sumOfSentiments / totalSentiments : 0
+
+    //   this.$refs.youtubeSentiment.innerHTML = `<span style="color: #ccc9dc; border: 1px solid #8ea7e9; font-weight:bold; border-radius: 50%; padding: 7px">${averageSentiment.toFixed(
+    //     2
+    //   )}</span>`
+    // },
     toggleFAQ2() {
       var answers = document.getElementsByClassName('faq-answer-2')
       for (var i = 0; i < answers.length; i++) {
@@ -154,6 +184,24 @@ export default {
           answers[i].style.display = 'none'
         }
       }
+
+      // Comment sentimenterini toplamak ve ortalama hesaplamak için
+      const sentimentList = this.movieObj.movieComments.redditComments
+        .filter((comment) => comment.sentiment !== undefined) // Filter out comments without sentiment
+        .map((comment) => comment.sentiment)
+
+      const totalSentiments = sentimentList.length
+      const sumOfSentiments = sentimentList.reduce(
+        (sum, sentiment) => sum + parseFloat(sentiment),
+        0
+      )
+
+      // Check if there are comments with sentiment to avoid division by zero
+      const averageSentiment = totalSentiments > 0 ? sumOfSentiments / totalSentiments : 0
+
+      this.$refs.redditSentiment.innerHTML = `<span style="color: #ccc9dc; border: 1px solid #8ea7e9; font-weight:bold; border-radius: 50%; padding: 7px">${averageSentiment.toFixed(
+        2
+      )}</span>`
     },
     formatDate(value) {
       const options = { day: 'numeric', month: 'short', year: 'numeric' }
@@ -212,7 +260,9 @@ export default {
 }
 
 .faq-answer-2 {
-  display: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   padding: 5px;
   padding-left: 3%;
   padding-right: 3%;
@@ -223,6 +273,15 @@ export default {
   width: 90%;
   margin-left: 5%;
   color: #ccc9cc;
+}
+
+.left-aligned {
+  float: right;
+  margin-top: 0.5%;
+  margin-left: 5%;
+  border-radius: 50%;
+  color: #d1b47a;
+  font-weight: bold;
 }
 .sentiment-analyze {
   color: #ccc9cc;
